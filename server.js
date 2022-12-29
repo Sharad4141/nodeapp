e93e088be98d64b1973c3e66a9598fef71f13f6a
dac <dac@dypm.org>
2022-12-29 23:51:43 +0530

    sharac

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require ('express');
+var app = express();
+var path = require ('path');
+
+app.use(express.static(path.join(__dirname,"public")));
+
+app.get('/', function(req,resp){
+    resp.sendFile(path.join(__dirname+ './index.html'));
+})
+app.listen(3000);
+console.log("server is on");
