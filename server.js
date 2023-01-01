05b618f6907dfce9151a91f027444b034d3b5d63
Sharad <sharad_shinde>
2023-01-02 00:24:25 +0530

    noeapp

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ app.use(express.static(path.join(__dirname,"public")));
 app.get('/', function(req,resp){
     resp.sendFile(path.join(__dirname+ './index.html'));
 })
-app.listen(3000);
+app.listen(1000);
 console.log("server is on");
